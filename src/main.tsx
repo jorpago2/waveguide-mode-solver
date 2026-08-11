4011caab22887e126c44da9ad8a5f40fb6cd8325
Jorge Parra <jorge.parra@uv.es>
2026-08-11 17:45:41 +0200

    Adopt scientific UI provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { GlobalTheme } from "@carbon/react";
+import { ScientificUiProvider } from "@jorpago2/scientific-ui";
 import { App } from "./App";
 import "./carbon.scss";
 import "./styles.css";
 import "@jorpago2/scientific-ui/styles.css";
 
-document.documentElement.classList.add("cds--g10");
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <GlobalTheme theme="g10"><App /></GlobalTheme>
+    <ScientificUiProvider><App /></ScientificUiProvider>
   </StrictMode>,
 );
